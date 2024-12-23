050a130cbbe130cb039604b7b5bbf5d80cd1f34a
MTPE <mtpe2301@gmail.com>
2024-12-23 15:28:33 +0700

    v1.5

--- backend/database/database.go
+++ backend/database/database.go
@@ -12,7 +12,6 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	// Attempt to remove the existing database file
 	err := os.Remove("tasksmnaa.db")
 	if err != nil && !os.IsNotExist(err) {
 		log.Fatalf("Failed to remove existing database file: %v", err)
@@ -23,7 +22,6 @@ func InitDB() {
 		panic("Failed to connect to database!")
 	}
 
-	// Automatically migrate the schema
 	err = DB.AutoMigrate(&models.Task{})
 	if err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
